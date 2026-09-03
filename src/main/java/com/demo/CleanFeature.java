dccc776485d70e71f1a7288394d8ab58b0fd15ce
batuhan4644 <batuhan4644@gmail.com>
2026-09-03 16:37:41 +0300

    Add clean coe + vulnerability

---
+++ src/main/java/com/demo/CleanFeature.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class CleanFeature {
+
+    public static void main(String[] args) {
+
+        String username = "Batuhan";
+
+        System.out.println("Username: " + username);
+    }
+}
