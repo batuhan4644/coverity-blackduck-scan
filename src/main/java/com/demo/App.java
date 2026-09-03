049c10543e5e0f771c2244af5c3b507e1723d231
batuhan4644 <batuhan4644@gmail.com>
2026-09-03 15:09:28 +0300

    Initial Coverity project

---
+++ src/main/java/com/demo/App.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        String username = "Batuhan";
+
+        System.out.println("Username: " + username);
+    }
+}
