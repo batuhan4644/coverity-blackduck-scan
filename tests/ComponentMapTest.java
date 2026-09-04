27c258fb7790214553ebc7e35b4ec1e70e57aeb8
batuhan4644 <batuhan4644@gmail.com>
2026-09-04 11:17:42 +0300

    Add component map test file

---
+++ tests/ComponentMapTest.java
@@ -0,0 +1,9 @@
+public class ComponentMapTest {
+    public static void main(String[] args) {
+        String a = new String("test");
+
+        if (a == "test") {
+            System.out.println("matched");
+        }
+    }
+}
