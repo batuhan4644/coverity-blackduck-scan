5ae9753ddf923eaf10abda27aa90f1d6f3b8c991
batuhan4644 <batuhan4644@gmail.com>
2026-09-04 10:22:37 +0300

    Test Coverity tests path exclusion

---
+++ src/main/java/com/demo/NormalVulnerable.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class NormalVulnerable {
+
+    public static void main(String[] args) {
+
+        String customer = null;
+
+        System.out.println(customer.length());
+    }
+}
---
+++ src/main/java/com/demo/tests/ExcludedVulnerable.java
@@ -0,0 +1,11 @@
+package com.demo.tests;
+
+public class ExcludedVulnerable {
+
+    public static void main(String[] args) {
+
+        String username = null;
+
+        System.out.println(username.length());
+    }
+}
